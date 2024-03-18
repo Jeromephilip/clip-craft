1a702ed9f75d9e6688edeb9306c2475d0ef319e2
Jeromephilip <jeromephilip03@gmail.com>
2024-03-18 09:32:58 -0400

    barebones image generator complete

--- src/image_generator.py
+++ src/image_generator.py
@@ -4,7 +4,31 @@ import os
 from dotenv import load_dotenv
 from PIL import Image
 
-driver = webdriver.Chrome()
+class Reddit_Image_Generator:
+
+    def __init__(self, url, username, password, client_id, secret_token):
+        self.username = username
+        self.password = password
+        self.client_id = client_id
+        self.secret_token = secret_token
+        self.url = url
+        self.image = None
+        self.cropped_image = None
+
+    def generate_image(self):
+        driver = webdriver.Chrome()
+        driver.get(self.url)
+        driver.save_screenshot('../assets/screenshots/screenshot.png')
+        self.image = Image.open('../assets/screenshots/screenshot.png')
+        return self.image.show()
+
+    def crop_image(self):
+        self.generate_image()
+        left, right, top, bottom = 260, 870, 60, 281 # manually calculated values
+        if (self.image):
+            cropped_image = self.image.crop((left, top, right, bottom))
+            return cropped_image.show()
+        return None
 
 load_dotenv()
 
@@ -15,14 +39,9 @@ SECRET_TOKEN = os.environ.get('SECRET_TOKEN')
 
 rq = Reddit_Request(USERNAME, PASSWORD, CLIENT_ID, SECRET_TOKEN)
 rq.search_subreddit("Showerthoughts")
+url = rq.get_dataframe_row(10)['url']
 
-url = rq.get_dataframe_row(2)['url']
+ig = Reddit_Image_Generator(url, USERNAME, PASSWORD, CLIENT_ID, SECRET_TOKEN)
+ig.crop_image()
 
-driver.get(url)
-
-driver.save_screenshot('../assets/screenshots/screenshot.png')
-
-image = Image.open('../assets/screenshots/screenshot.png')
-
-image.show()
 
