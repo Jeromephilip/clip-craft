0b18b23c9b8acbf03d886c7d432ef0b8ffdfefff
Jeromephilip <jeromephilip03@gmail.com>
2024-06-20 22:48:59 -0400

    tts tiktok

--- src/tts.py
+++ src/tts.py
@@ -1,10 +1,10 @@
 from gtts import gTTS
 import os
+from tiktokvoice import tts
+
 
 # TODO: replace gTTS with TikTok standard voice
 
 def speak(text):
-    mytext = text
-    language = 'en'
-    myobj = gTTS(text=mytext, lang=language, slow=False)
-    myobj.save("../assets/tts/tts.mp3")
+    voice = 'en_us_009'
+    tts(text, voice, "../assets/tts/tts.mp3", play_sound=True)
